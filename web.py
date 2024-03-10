936885c2ccacef5169f9df4864ca9adab5e85b32
Raymond <raymondli631@qq.com>
2024-03-10 21:49:39 +0800

    不在名单内禁止提交

--- web.py
+++ web.py
@@ -32,16 +32,18 @@ def entrypoint():
 def submit():
     data = request.get_json()
     print(f"收到数据: {data}")
+    if data.get("name") not in people.keys():
+        return '用户名不存在'
+
     # 插入redis（重复提交可以覆盖）
     length = utils.put_into_redis(data)
-
     if length >= num_students:
         print("收集完毕")
         # 从redis取出所有数据
         collected_data = utils.get_from_redis()
         thread = threading.Thread(target=Scheduler.start, args=(collected_data, num_students, num_classes))
         thread.start()
-    return 'OK'
+    return '提交成功'
 
 
 @app.route('/look', methods=['get'])
